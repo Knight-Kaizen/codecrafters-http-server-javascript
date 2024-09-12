b7b7c142a41cdc39cbaf14d35ccb9fcc9bc539e0
Knight-Kaizen <auferstehung20@gmail.com>
2024-09-12 07:57:25 +0530

    add file read

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const net = require("net");
 const path = require("path");
+const fs = require("fs");
 
 // create a tcp serverx
 const server = net.createServer((socket) => {
@@ -13,40 +14,57 @@ const server = net.createServer((socket) => {
 
         // Index 0 -> contains request line, Index n-1 -> contains req body , Index n-2 contains '', rest all are headers
         const requestLine = data[0];
-        const reqBody = data[data.length -1];
-        const headers = data.slice(1, data.length-2);
+        const reqBody = data[data.length - 1];
+        const headers = data.slice(1, data.length - 2);
 
-        // 3. request line consists of http method at index 0, path at 1, http vetrsion at 2nd index. 
+        // 3. request line consists of http method at index 0, url at 1, http vetrsion at 2nd index. 
         const reqMethod = requestLine.split(' ')[0];
-        let path = requestLine.split(' ')[1];
+        let url = requestLine.split(' ')[1];
 
-        // 4. If the path is / pass the req, else fail 
-        if (path == '/') {
+        // 4. If the url is / pass the req, else fail 
+        if (url == '/') {
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
         }
-        else if(path.includes('/echo')){
+        else if (url.includes('/echo')) {
 
-            path = path.split('/');
+            url = url.split('/');
 
-            if(path.length >= 3){
-                const str = path[2];
+            if (url.length >= 3) {
+                const str = url[2];
                 // 4. Write this in response body
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
             }
             else
-            socket.write("HTTP/1.1 200 OK\r\n\r\n")
+                socket.write("HTTP/1.1 200 OK\r\n\r\n")
         }
-        else if(path.includes('/user-agent')){
-            for(const header of headers){
+        else if (url.includes('/user-agent')) {
+            for (const header of headers) {
 
                 const headerKey = header.split(':')[0];
                 const headerValue = header.split(' ')[1];
 
-                if(headerKey == "User-Agent"){
+                if (headerKey == "User-Agent") {
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${headerValue.length}\r\n\r\n${headerValue}`);
                 }
-            
+
             }
+        }
+        else if (url.includes('/files')) {
+
+            const directory = process.argv[3] ? process.argv[3] : "files";
+            const filename = url.split('/')[2];
+
+            const fileurl = path.join(process.cwd(), directory, filename);
+            if(fs.existsSync(fileurl)){
+                const fileBuffer = fs.readFileSync(fileurl);
+                const content = fileBuffer.toString();
+
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+            }
+            else{
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+            }
+
         }
         else
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
