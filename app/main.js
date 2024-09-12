f2fbaa66e423df3ab6a5de26fd9fec4f80484ffd
Knight-Kaizen <auferstehung20@gmail.com>
2024-09-12 11:24:03 +0530

    add header for compressing data

--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 const net = require("net");
 const path = require("path");
 const fs = require("fs");
-const { dir } = require("console");
 
 // create a tcp serverx
 const server = net.createServer((socket) => {
@@ -18,6 +17,7 @@ const server = net.createServer((socket) => {
         const reqBody = data[data.length - 1];
         const headers = data.slice(1, data.length - 2);
 
+
         // 3. request line consists of http method at index 0, url at 1, http vetrsion at 2nd index. 
         const reqMethod = requestLine.split(' ')[0];
         let url = requestLine.split(' ')[1];
@@ -29,11 +29,26 @@ const server = net.createServer((socket) => {
         else if (url.includes('/echo')) {
 
             url = url.split('/');
+            let reqCompressionHeader = '';
+            let reqCompressionHeaderValue = '';
+            let resCompressionHeader = '';
+
+            for (const header of headers) {
+                const headerKey = header.split(':')[0];
+                const headerValue = header.split(' ')[1];
+
+                if (headerKey == 'Accept-Encoding' && headerValue == 'gzip') {
+                    reqCompressionHeader = headerKey;
+                    reqCompressionHeaderValue = headerValue;
+                    resCompressionHeader = "Content-Encoding";
+                }
+            }
 
             if (url.length >= 3) {
                 const str = url[2];
                 // 4. Write this in response body
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
+                const compressionHeader = (reqCompressionHeader) ? `${resCompressionHeader}: ${reqCompressionHeaderValue}\r\n` : '';
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n${compressionHeader}\r\n${str}`);
             }
             else
                 socket.write("HTTP/1.1 200 OK\r\n\r\n")
@@ -51,32 +66,32 @@ const server = net.createServer((socket) => {
             }
         }
         else if (url.includes('/files')) {
-            if(reqMethod == "GET"){
+            if (reqMethod == "GET") {
                 const directory = process.argv[3];
                 const filename = url.split('/')[2];
-    
+
                 const fileurl = path.join(directory, filename);
-                if(fs.existsSync(fileurl)){
+                if (fs.existsSync(fileurl)) {
                     const fileBuffer = fs.readFileSync(fileurl);
                     const content = fileBuffer.toString();
-    
+
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
                 }
-                else{
+                else {
                     socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
                 }
             }
-            else if(reqMethod == "POST"){
+            else if (reqMethod == "POST") {
                 const directory = process.argv[3];
                 const filename = url.split('/')[2];
-                
+
                 const fileurl = path.join(directory, filename);
-                if(!fs.existsSync(directory))
+                if (!fs.existsSync(directory))
                     fs.mkdirSync(directory)
-                
+
                 const content = reqBody;
                 fs.writeFileSync(fileurl, content);
-                
+
                 socket.write('HTTP/1.1 201 Created\r\n\r\n');
             }
 
