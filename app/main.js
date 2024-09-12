3cad6a54ac2d5e72decdf8efe7e3dac00a072cc3
Knight-Kaizen <auferstehung20@gmail.com>
2024-09-12 07:22:32 +0530

    add user-agent route to read headers and send them back in response body

--- app/main.js
+++ app/main.js
@@ -9,11 +9,16 @@ const server = net.createServer((socket) => {
         data = data.toString();
 
         // 2. Split data based on http parts, (1st part is request line, 2nd one is headers and 3rd is optional request body) all seperated by CRLF (\r\n)
-        data = data.split('\r\n')[0];
+        data = data.split('\r\n');
+
+        // Index 0 -> contains request line, Index n-1 -> contains req body , Index n-2 contains '', rest all are headers
+        const requestLine = data[0];
+        const reqBody = data[data.length -1];
+        const headers = data.slice(1, data.length-2);
 
         // 3. request line consists of http method at index 0, path at 1, http vetrsion at 2nd index. 
-        const method = data.split(' ')[0];
-        let path = data.split(' ')[1];
+        const reqMethod = requestLine.split(' ')[0];
+        let path = requestLine.split(' ')[1];
 
         // 4. If the path is / pass the req, else fail 
         if (path == '/') {
@@ -30,6 +35,18 @@ const server = net.createServer((socket) => {
             }
             else
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        }
+        else if(path.includes('/user-agent')){
+            for(const header of headers){
+
+                const headerKey = header.split(':')[0];
+                const headerValue = header.split(' ')[1];
+
+                if(headerKey == "User-Agent"){
+                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${headerValue.length}\r\n\r\n${headerValue}`);
+                }
+            
+            }
         }
         else
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
